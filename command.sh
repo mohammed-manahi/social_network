e828571e6ede5b5e69a86df4037c5591b527ae59
Mohammed Manahi <mohammedsaadmanahi@hotmail.com>
2023-04-17 01:25:32 +0300

    Initial commit

---
+++ command.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+# Collect static files command
+python3 manage.py collectstatic --noinput
+
+# Apply migrations command
+python3 manage.py migrate
+
+# Start server command
+python3 manage.py runserver 0.0.0.0:8000
---
+++ launch.sh
@@ -0,0 +1,4 @@
+# Create social network project
+django-admin startproject social_network .
+# Create core App
+django-admin startapp core
